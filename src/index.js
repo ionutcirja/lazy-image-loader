3c8f36a2aa1a85adec5c3b6cb14e201c767d4989
Ionut Cirja <ionut.cirja@sapient.com>
2018-12-06 08:56:37 +0000

    refactoring / plug in flow

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 /* eslint-disable no-use-before-define */
+// @flow
 
-export default src =>
-    new Promise((resolve, reject) => {
-        const image = document.createElement('img');
+export default (src: string): Promise<*> => (
+  new Promise((resolve, reject) => {
+    const image: HTMLImageElement = document.createElement('img');
 
-        const loadComplete = () => {
-            removeListeners(image);
-            resolve({ width: image.width, height: image.height });
-        };
-        const loadError = () => {
-            removeListeners(image);
-            reject('Image loading error');
-        };
-        const addListeners = () => {
-            image.addEventListener('load', loadComplete);
-            image.addEventListener('error', loadError);
-        };
-        const removeListeners = () => {
-            image.removeEventListener('load', loadComplete);
-            image.removeEventListener('error', loadError);
-        };
+    const loadComplete = () => {
+      removeListeners();
+      resolve({ width: image.width, height: image.height });
+    };
+    const loadError = () => {
+      removeListeners();
+      reject(Error('Image cannot be loaded.'));
+    };
+    const addListeners = () => {
+      image.addEventListener('load', loadComplete);
+      image.addEventListener('error', loadError);
+    };
+    const removeListeners = () => {
+      image.removeEventListener('load', loadComplete);
+      image.removeEventListener('error', loadError);
+    };
 
-        addListeners(image);
-        image.src = src;
+    addListeners();
+    image.src = src;
 
-        if (image.complete) {
-            removeListeners(image);
-            resolve({ width: image.width, height: image.height });
-        }
-    });
+    if (image.complete) {
+      loadComplete();
+    }
+  })
+);
